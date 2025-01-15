86882fd9d224837d9e60ffe26c985a358ce3a426
Anthony Fong <FongAnthonyM@gmail.com>
2025-01-14 17:14:25 -0800

    Started setting pickling function feature

--- src/blockobjects/process/context/multiprocessingcontext/functionreducer.py
+++ src/blockobjects/process/context/multiprocessingcontext/functionreducer.py
@@ -15,6 +15,7 @@ __email__ = __email__
 # Standard Libraries #
 from collections.abc import Callable
 from multiprocessing.reduction import ForkingPickler
+from functools import partial
 from types import FunctionType
 from typing import Any
 
@@ -25,8 +26,8 @@ import dill
 
 
 # Definitions #
-# Classes #
-def reducefunction(function: Callable) -> tuple[type, tuple[Any, ...]]:
+# function #
+def reducefunction(function: Callable) -> tuple[Callable, tuple[Any, ...]]:
     """Reduces a callable serialized object containing the build function and its arguments.
 
     Args:
@@ -35,8 +36,41 @@ def reducefunction(function: Callable) -> tuple[type, tuple[Any, ...]]:
     Returns:
         A tuple with the build function and the arguments for the build function.
     """
-    return dill.loads, (dill.dumps(function),)
+    return rebuildfunction, (dill.dumps(function),)
+
+
+def rebuildfunction(dill_function: bytes) -> Callable:
+    """Rebuilds a callable serialized object containing the build function and its arguments.
+
+    Args:
+        dill_function: The serialized callable object to be rebuilt.
+
+    Returns:
+        The rebuilt callable object.
+    """
+    return dill.loads(dill_function)
+
+
+def reducer_override(self, obj: Any) -> tuple[type, tuple[Any, ...]] | type[NotImplemented]:
+    if isinstance(obj, FunctionType) and obj.__name__ != "<lambda>":
+        return reducefunction(obj)
+    else:
+        return NotImplemented
+
+
+def reducer_override_override(
+    self,
+    obj: Any,
+    parent_method: Callable,
+) -> tuple[type, tuple[Any, ...]] | type[NotImplemented]:
+    if not isinstance(obj, FunctionType) or obj in self.dispatch_table:
+        return parent_method(self, obj)
+    else:
+        return reducefunction(obj)
 
 
 # Registration #
-ForkingPickler.register(FunctionType, reducefunction)
+if (method := getattr(ForkingPickler, "reducer_override", None)) is None:
+    ForkingPickler.reducer_override = reducer_override
+else:
+    ForkingPickler.reducer_override = partial(reducer_override_override, parent_method=method)
