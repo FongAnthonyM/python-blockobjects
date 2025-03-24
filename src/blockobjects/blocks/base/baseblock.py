9c3bd57e5ad8c111bc21d639a09fd84557347465
Anthony Fong <FongAnthonyM@gmail.com>
2025-03-24 14:46:41 -0700

    Changed how default signals are loaded to ensure they are present when creating the block

--- src/blockobjects/blocks/base/baseblock.py
+++ src/blockobjects/blocks/base/baseblock.py
@@ -635,7 +635,7 @@ class BaseBlock(ProcessArbitrator, CallableMultiplexObject):
         await self.inputs.set_io_async(self.input_link_name, io_wrapper)
 
     def set_signal_links(self) -> None:
-        for name, entry in self.signal_callback_map.items():
+        for name, entry in (self._signal_callback_map_ | self.signal_callback_map).items():
             method = entry["method"]
             method_async = entry.get("method_async", None)
             io_wrapper = self.create_io_wrapper(put=method, put_async=method_async, as_proxy=True)
@@ -644,7 +644,7 @@ class BaseBlock(ProcessArbitrator, CallableMultiplexObject):
     async def set_signal_links_async(self) -> None:
         # Create Input Change Tasks
         tasks = deque()
-        for name, entry in self.signal_callback_map.items():
+        for name, entry in (self._signal_callback_map_ | self.signal_callback_map).items():
             method = entry["method"]
             method_async = entry.get("method_async", None)
             io_wrapper = self.create_io_wrapper(put=method, put_async=method_async, as_proxy=True)
